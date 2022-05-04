40a30d86f23da01c2ff9cb70cc6d78e343ad2dc2
junyeongchang <jychang11@naver.com>
2022-05-04 15:22:10 +0900

    20220504

---
+++ UI_10_Active_04/scroll.js
@@ -0,0 +1,20 @@
+// Document Object Model = DOM
+
+//현재 화면의 모든 요소들이 다 그려지면, 완성되면
+//js코드를 시작하겠다라는 의미
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector("header");
+  const headerFake = document.querySelector("header.fake");
+  const section = document.querySelector("section");
+
+  // 화면의 스크롤이 발생하면 실행하기
+  document.addEventListener("scroll", () => {
+    const windowTop = window.scrollY;
+
+    if (windowTop > 150) {
+      header.classList.add("scroll");
+    } else {
+      header.classList.remove("scroll");
+    }
+  });
+});
